abb514504638bd50d0a8dee7aaaa4940bb8fb27f
MainShayne233 <shaynetremblay@hotmail.com>
2016-06-26 12:13:37 -0400

    init init

---
+++ lib/sheet_builder.rb
@@ -0,0 +1,5 @@
+require "sheet_builder/version"
+
+module SheetBuilder
+  # Your code goes here...
+end
---
+++ spec/sheet_builder_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SheetBuilder do
+  it 'has a version number' do
+    expect(SheetBuilder::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
